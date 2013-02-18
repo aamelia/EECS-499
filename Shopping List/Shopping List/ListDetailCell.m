a6caf5b2a31ad4949ad2245c80beb57ea4f35b07
aamelia <aamelia@umich.edu>
2013-02-18 12:57:24 -0500

    no message

---
+++ Shopping List/Shopping List/ListDetailCell.m
@@ -0,0 +1,29 @@
+//
+//  ListDetailCell.m
+//  Shopping List
+//
+//  Created by Mia Atkinson on 2/18/13.
+//  Copyright (c) 2013 499. All rights reserved.
+//
+
+#import "ListDetailCell.h"
+
+@implementation ListDetailCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
