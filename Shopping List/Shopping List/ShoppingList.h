b6c66f40998eed40111eef2fdf6176125c81e8c4
aamelia <aamelia@umich.edu>
2013-02-18 22:28:06 -0500

    no message

--- Shopping List/Shopping List/ShoppingList.h
+++ Shopping List/Shopping List/ShoppingList.h
@@ -1,13 +1,13 @@
 //
-//  ItemDetailViewController.h
+//  ShoppingList.h
 //  Shopping List
 //
 //  Created by Mia Atkinson on 2/18/13.
 //  Copyright (c) 2013 499. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ItemDetailViewController : UIViewController
+@interface ShoppingList : NSObject
 
 @end
---
+++ Shopping List/Shopping List/ShoppingList.m
@@ -0,0 +1,13 @@
+//
+//  ShoppingList.m
+//  Shopping List
+//
+//  Created by Mia Atkinson on 2/18/13.
+//  Copyright (c) 2013 499. All rights reserved.
+//
+
+#import "ShoppingList.h"
+
+@implementation ShoppingList
+
+@end
